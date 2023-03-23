fb25c8b4ebec92d0971dcf60d23b1b303896d2e4
Patrick Rezende <96663359+PatrickRez@users.noreply.github.com>
2023-03-22 23:29:44 -0300

    Finalizado
    HTML / CSS / Js / Google Font/ iconicon
    
    -Finalizado e testado
    -Fazer deploy

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 const form = document.getElementById("formAtividade");
 
-let linha = " ";
+let linhas = " ";
 const nomeContato = [];
 const numeroContato = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    adicionaLinhas();
+    adicionaLinha();
     atualizaTabela();
 });
 
 function atualizaTabela() {
     const inputNovoName = document.getElementById('NomeContato');
     const inputNovoNumber = document.getElementById('NumeroContato');
-    
+
     if (nomeContato.includes(inputNovoName.value)) {
         alert(`Este contato ja foi inserido ${inputNovoName.value} já foi inserida!`)
 
-        
+
     } else {
-        contato.push(inputNovoName.value);
-        
-        
+        nomeContato.push(inputNovoName.value);
+        numeroContato.push(parseInt(inputNovoNumber.value));
+
+
         let linha = '<tr>';
         linha += `<td>${inputNovoName.value} </td> `;
         linha += `<td>${inputNovoNumber.value}</td>`;
         linha += '</tr>';
 
         linhas += linha;
-
-        function atualizaTabela() {
-
-            const corpoTabela = document.querySelector('tbody');
-        
-            corpoTabela.innerHTML = linhas;
-        }
-    }
-    function adicionaLinha() {
-        nomeContato.push(inputNovoName.value);
-        numeroContato.push(parseInt(inputNovoNumber.value));
     }
 }
+function adicionaLinha() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
